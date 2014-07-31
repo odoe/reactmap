f47fed20cd1033f9f997654c69f64a98f1c1c517
odoe <odoe@odoe.net>
2014-07-31 13:25:16 -0700

    added options to add a FeatureLayer to map

--- main.js
+++ main.js
@@ -21,8 +21,33 @@
       this.setState({
         map: L.map(this.refs.mapDiv.getDOMNode())
       });
-      this.state.map.setView(this.props.coordinates, this.props.zoom);
-      L.esri.basemapLayer(this.props.basemap).addTo(this.state.map);
+
+      this.addBaseMap(
+        this.state.map.setView(this.props.coordinates, this.props.zoom)
+      );
+
+      if (this.props.layers) {
+        this.props.layers.map(function(layer) {
+          if (layer.type === 'feature') {
+            var params = layer.params || {};
+            this.addFeatureLayer(layer, params)
+              .addTo(this.state.map);
+          }
+        }.bind(this));
+      }
+
+    },
+
+    addFeatureLayer: function(options, params) {
+      var layer = new L.esri.FeatureLayer(options.url, params);
+      if (options.bindPopupMethod) {
+        layer.bindPopup(options.bindPopupMethod);
+      }
+      return layer;
+    },
+
+    addBaseMap: function(m) {
+      L.esri.basemapLayer(this.props.basemap).addTo(m);
     },
 
     render: function() {
@@ -39,7 +64,19 @@
     <ReactMap
       coordinates={[45.528, -122.680]}
       zoom={13}
-      basemap={'Gray'}/>, document.body
+      basemap={'Gray'}
+      layers={[{
+        type: 'feature',
+        url: 'http://services.arcgis.com/rOo16HdIMeOBI4Mb/arcgis/rest/services/Portland_Parks/FeatureServer/0',
+        params: {
+          style: function() {
+            return { color: '#70ca49', weight: 2 };
+          }
+        },
+        bindPopupMethod: function(feature) {
+          return L.Util.template('<h3>{NAME}</h3>{ACRES} Acres<br><small>Property ID: {PROPERTYID}<small>', feature.properties);
+        }
+      }]}/>, document.body
   );
 
 
